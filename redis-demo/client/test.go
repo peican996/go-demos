b1a9516e03012a77cbf7fe4d5fdadc868c30f504
peican996 <17756311926@163.com>
2023-06-11 01:26:23 +0800

    feat:redis缓存耗时和sql查询耗时对比

--- redis-demo/client/test.go
+++ redis-demo/client/test.go
@@ -5,19 +5,29 @@ import (
 	"demos/redis-demo/redis"
 	"encoding/json"
 	"fmt"
+	"time"
 )
 
 func main() {
-	str := redis.Client.Get("user:218")
-	if str.Val() != "" {
-		// 解析JSON数据到结构体
-		var user model.User
-		err := json.Unmarshal([]byte(str.Val()), &user)
-		if err != nil {
-			fmt.Println("解析JSON失败:", err)
+	start := time.Now()
+	for i := 0; i < 10000; i++ {
+		str := redis.Client.Get("user:217")
+		if str.Val() != "" {
+			// 解析JSON数据到结构体
+			var user model.User
+			err := json.Unmarshal([]byte(str.Val()), &user)
+			if err != nil {
+				fmt.Println("解析JSON失败:", err)
+			}
 		}
-		return
 	}
-	user := model.GetUserByID("218")
-	redis.CacheValue(user)
+	cost := time.Since(start)
+	fmt.Println("cost=", cost)
+	start1 := time.Now()
+	for i := 0; i < 10000; i++ {
+		model.GetUserByID("218")
+	}
+	cost1 := time.Since(start1)
+	fmt.Println("cost=", cost1)
+	//redis.CacheValue(user)
 }
